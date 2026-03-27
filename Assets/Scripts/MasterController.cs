1b9c58264b97cf3cd9add54437d53d9c18f105d6
Adi Shaked <ashaked84@gmail.com>
2026-03-27 16:34:05 +0300

    Player can now adjust master volume, music volume, and sound effect volume seperately in the settings menu.

--- Assets/Scripts/MasterController.cs
+++ Assets/Scripts/MasterController.cs
@@ -5,8 +5,18 @@ public class MasterController : MonoBehaviour
 {
     [SerializeField] private AudioMixer myAudioMixer;
 
-    public void SetVolume(float sliderValue)
+    public void SetMasterVolume(float value)
     {
-        myAudioMixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+        myAudioMixer.SetFloat("MasterVolume", Mathf.Log10(value) * 20);
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        myAudioMixer.SetFloat("MusicVolume", Mathf.Log10(value) * 20);
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        myAudioMixer.SetFloat("SFXVolume", Mathf.Log10(value) * 20);
     }
 }
